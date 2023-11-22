93451b75c553dc4d426e56024aa3918eef793bbe
EngineerDami <a1432108650@gmail.com>
2023-11-22 09:34:05 +0800

    NavBar and Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import { TbBrandDiscord } from 'react-icons/tb';
+
+function Footer() {
+	return (
+		<>
+			<footer className='footer footer-center p-10 bg-base-200 text-base-content border-base-300 rounded'>
+				<div className='flex-1 px-2 mx-2'>
+					<div className='relative flex h-12 items-center justify-between'>
+						<p className='relative text-base-content'>Follow us on</p>
+
+						<Link to='/' className='btn btn-sm rounded-btn '>
+							<div class='fa-bilibili'></div>Bilibili
+						</Link>
+						<Link to='/porfolio' className='btn btn-sm rounded-btn'>
+							<div class='fa-kook'></div>KOOK
+						</Link>
+						<Link to='/resources' className='btn btn-sm rounded-btn '>
+							<TbBrandDiscord className='h-5 w-5' />
+							Resources
+						</Link>
+					</div>
+				</div>
+			</footer>
+
+			<footer className='footer footer-center border-t-2 border-gray-400 px-10 pt-7 pb-5 bg-base-200 text-base-content border-base-300'>
+				<aside>
+					<p className='pb-3'>
+						Copyright © 2023 - All right reserved by Engineerdami
+					</p>
+					<p>
+						<strong>
+							Thie site is specified for website deployment and fully
+							customization as self-learning purposes only.
+						</strong>
+					</p>
+				</aside>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,44 @@
+import { FaGithub } from 'react-icons/fa';
+
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
+
+function Navbar({ title }) {
+	return (
+		<nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content fixed'>
+			<div className='container mx-auto'>
+				<div className='flex-none px-2 mx-2'>
+					<FaGithub className='inline pr-2  text-3xl' />
+					<Link to='/' className='text-lg font-bold align-middle'>
+						{title}
+					</Link>
+				</div>
+
+				<div className='flex-1 px-2 mx-2'>
+					<div className='flex justify-end'>
+						<Link to='/' className='btn btn-ghost btn-sm rounded-btn '>
+							Home
+						</Link>
+						<Link to='/porfolio' className='btn btn-ghost btn-sm rounded-btn '>
+							Porfolio
+						</Link>
+						<Link to='/resources' className='btn btn-ghost btn-sm rounded-btn '>
+							Resources
+						</Link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+Navbar.defaultProps = {
+	title: 'Github Search',
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string,
+};
+
+export default Navbar;
